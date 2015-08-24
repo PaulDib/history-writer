65ecb55ad803976cfe67670edb96344a0f02bd8e
Paul Dib <paul.dib@orange.fr>
2015-08-24 15:14:22 +0200

    Add character grids for letters

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+extern crate time;
+
 use std::collections::HashMap;
 use std::env;
 
+mod char_grid;
+
+use char_grid::CharGrid;
 
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 enum Parameter {
@@ -33,14 +38,32 @@ fn read_cli_arguments() -> HashMap<Parameter, String> {
     parameters
 }
 
-fn print_parameters(parameters: &HashMap<Parameter, String>) {
-    for (key, value) in parameters {
-        println!("[{:?}] -> {}", key, value);
-    }
+fn get_next_sunday(t: time::Tm) -> time::Tm {
+    let days: i32 = 7 - t.tm_wday;
+    t + time::Duration::days(days as i64)
+}
+
+fn get_sunday_of_last_year(t: time::Tm) -> time::Tm {
+    get_next_sunday(get_next_sunday(t) - time::Duration::days(365))
 }
 
 fn main() {
     let params = read_cli_arguments();
-    print_parameters(&params);
+    println!("{}", time::now().rfc822());
+    println!("{}", get_sunday_of_last_year(time::now()).rfc822());
+    let cg = CharGrid::new();
+    let s = "paul";
+    for c in s.chars() {
+        for w in cg.get_grid_for(c) {
+            for i in w {
+                if i > &0 {
+                    print!("x");
+                } else {
+                    print!(".");
+                }
+            }
+            println!("\n");
+        }
+    }
 }
 
