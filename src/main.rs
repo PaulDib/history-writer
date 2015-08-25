c9f1a0b85553843ae144599bfa79fd9a8bde60af
Paul Dib <paul.dib@orange.fr>
2015-08-25 17:11:51 +0200

    Add main routine to write in history

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+extern crate git2;
 extern crate time;
 
 use std::collections::HashMap;
@@ -10,14 +11,20 @@ use char_grid::CharGrid;
 #[derive(PartialEq, Eq, Hash, Clone, Copy, Debug)]
 enum Parameter {
     RepositoryName,
+    RepositoryPath,
     Text,
+    LowCommitCount,
+    HighCommitCount,
     None,
 }
 
 fn match_parameter(parameter: &str) -> Parameter {
     match parameter.as_ref() {
         "repo" => Parameter::RepositoryName,
+        "path" => Parameter::RepositoryPath,
         "text" => Parameter::Text,
+        "low" => Parameter::LowCommitCount,
+        "high" => Parameter::HighCommitCount,
         _ => panic!("Unknown parameter: <{}>", parameter),
     }
 }
@@ -43,27 +50,50 @@ fn get_next_sunday(t: time::Tm) -> time::Tm {
     t + time::Duration::days(days as i64)
 }
 
-fn get_sunday_of_last_year(t: time::Tm) -> time::Tm {
-    get_next_sunday(get_next_sunday(t) - time::Duration::days(365))
+fn get_sunday_n_weeks_before(t: time::Tm, n: i32) -> time::Tm {
+    get_next_sunday(get_next_sunday(t) - time::Duration::days((n*7) as i64))
+}
+
+fn write_to_repository(repo: &mut git2::Repository, low: i32, high: i32, s: String) {
+    let mut previous_commit: Option<git2::Commit> = None;
+    let mut commit_time = get_sunday_n_weeks_before(time::now(), (s.len()*4) as i32);
+    let oid = repo.index().unwrap().write_tree().unwrap();
+    let cg = CharGrid::new();
+    for c in s.chars() {
+        let array = cg.get_grid_for(c);
+        let height = array.len();
+        let width = array[0].len();
+        for j in 0..width {
+            for i in 0..height {
+                let count = if array[i][j] == 1 {
+                    high
+                } else {
+                    low
+                };
+                let t = git2::Time::new(commit_time.to_timespec().sec, 0);
+                let s = git2::Signature::new("Paul Dib", "paul.dib@orange.fr", &t).unwrap();
+                for _ in 0..count {
+                    let commit_id = match previous_commit {
+                        None => repo.commit(Some("HEAD"), &s, &s, " ", &repo.find_tree(oid).unwrap(), &[]).unwrap(),
+                        Some(commit) => repo.commit(Some("HEAD"), &s, &s, " ", &repo.find_tree(oid).unwrap(), &[&commit]).unwrap(),
+                    };
+                    previous_commit = repo.find_commit(commit_id).ok();
+                }
+                commit_time = commit_time + time::Duration::days(1);
+            }
+        }
+    }
 }
 
 fn main() {
     let params = read_cli_arguments();
-    println!("{}", time::now().rfc822());
-    println!("{}", get_sunday_of_last_year(time::now()).rfc822());
-    let cg = CharGrid::new();
-    let s = "paul";
-    for c in s.chars() {
-        for w in cg.get_grid_for(c) {
-            for i in w {
-                if i > &0 {
-                    print!("x");
-                } else {
-                    print!(".");
-                }
-            }
-            println!("\n");
-        }
-    }
+    
+    let mut repo = git2::Repository::init(&params[&Parameter::RepositoryPath]).unwrap();
+    
+
+    let low = params[&Parameter::LowCommitCount].parse::<i32>().unwrap();
+    let high = params[&Parameter::HighCommitCount].parse::<i32>().unwrap();
+    
+    write_to_repository(&mut repo, low, high, params[&Parameter::Text].clone());
 }
 
